be4c6b6002952ddcb0dfa89684bf525ef5055147
Tomasz Klim <github@tomaszklim.pl>
2018-08-10 12:09:32 +0200

    enforce custom groups

--- setup.sh
+++ setup.sh
@@ -13,3 +13,8 @@ if [ ! -f /root/.ssh/authorized_keys ] || ! grep -q "$FULLKEY" /root/.ssh/author
 	mkdir -p /root/.ssh
 	echo "$FULLKEY" >>/root/.ssh/authorized_keys
 fi
+
+echo "checking custom system groups"
+/opt/farm/ext/passwd-utils/create-group.sh mfs 140
+/opt/farm/ext/passwd-utils/create-group.sh sambashare 150
+/opt/farm/ext/passwd-utils/create-group.sh imapusers 160
