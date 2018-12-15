fdee9681ac5c3d89a14f9199291fe4d9ae020641
Tomasz Klim <github@tomaszklim.pl>
2018-12-15 23:16:57 +0100

    use sf-passwd-utils extension to manage root management key

--- setup.sh
+++ setup.sh
@@ -1,23 +1,16 @@
 #!/bin/sh
 . /opt/farm/scripts/init
 
+/opt/farm/scripts/setup/extension.sh sf-passwd-utils
 
-echo "preparing ssh key"
-FULLKEY=`/opt/farm/ext/keys/get-ssh-management-key-content.sh $HOST`
-
-if [ ! -f /root/.ssh/authorized_keys ] || ! grep -q "$FULLKEY" /root/.ssh/authorized_keys; then
-	echo "setting up root ssh key"
-	mkdir -p /root/.ssh
-	echo "$FULLKEY" >>/root/.ssh/authorized_keys
-fi
+key=`/opt/farm/ext/keys/get-ssh-management-key-content.sh $HOST`
+/opt/farm/ext/passwd-utils/add-key.sh root inline "$key"
 
 
 # TODO: Ubuntu 14.04 LTS and newer has Samba installed and group sambashare
 # created right after fresh install. Detect similar cases, change GID of this
 # group and chgrp directories previously group-owned by sambashare group.
 
-/opt/farm/scripts/setup/extension.sh sf-passwd-utils
-echo "checking custom system groups"
 /opt/farm/ext/passwd-utils/create-group.sh mfs 140
 /opt/farm/ext/passwd-utils/create-group.sh sambashare 150
 /opt/farm/ext/passwd-utils/create-group.sh imapusers 160
