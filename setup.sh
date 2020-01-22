91db66f06f98c936b5115e8c62472181535ac3e4
Tomasz Klim <github@tomaszklim.pl>
2020-01-23 00:38:31 +0100

    improved detection of proper ssh root keys to install on current host

--- setup.sh
+++ setup.sh
@@ -1,10 +1,29 @@
 #!/bin/sh
 . /opt/farm/scripts/init
 
-/opt/farm/scripts/setup/extension.sh sf-passwd-utils
+add_root_key() {
+	key=`/opt/farm/ext/keys/get-ssh-management-key-content.sh $1`
+	/opt/farm/ext/passwd-utils/add-key.sh root inline "$key"
+}
 
-key=`/opt/farm/ext/keys/get-ssh-management-key-content.sh $HOST`
-/opt/farm/ext/passwd-utils/add-key.sh root inline "$key"
+
+add_root_key $HOST
+
+# actual server hostname can differ from configured one
+current=`hostname`
+if [ "$HOST" != "$current" ]; then
+	add_root_key $current
+fi
+
+
+# TODO: detect also Microsoft Azure and Google Cloud (and only these)
+
+# detect Amazon EC2/ECS and install also keys for one of these
+if [ -f /etc/image-id ] && grep -q ami-ecs /etc/image-id; then
+	add_root_key ecs-null
+elif [ -d /sys/class/dmi/id ] && grep -qi amazon /sys/class/dmi/id/* 2>/dev/null; then
+	add_root_key .amazonaws.com
+fi
 
 
 # TODO: Ubuntu 14.04 LTS and newer has Samba installed and group sambashare
